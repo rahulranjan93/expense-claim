dceb9419b735e41ffd4e904936136f2cc4dbf0b5
Abhishek Garg <abhishek.garg@plangrid.com>
2019-12-24 17:13:17 +0530

    add get all teams route

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, request, jsonify, g, url_for
+from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -16,54 +16,6 @@ auth = HTTPBasicAuth()
 from app import routes, models
 from app.models import Employee
 
-
-@auth.verify_password
-def verify_password(username_or_token, password):
-    # first try to authenticate by token
-    print("aaaaaaaa",username_or_token)
-    employee = Employee.verify_auth_token(username_or_token)
-    if not employee:
-        # try to authenticate with email/password
-        employee = Employee.query.filter_by(email=username_or_token).first()
-        if not employee or not employee.verify_password(password):
-            return False
-    g.employee = employee
-    return True
-
-
-@app.route('/token')
-@auth.login_required
-def get_auth_token():
-    token = g.employee.generate_auth_token(600)
-    return jsonify({'token': token.decode('ascii'), 'duration': 600})
-
-
-@app.route('/resource')
-@auth.login_required
-def get_resource():
-    return jsonify({'data': 'Hello, %s!' % g.employee.email})
-
-@app.route("/number/<number>")
-def get_number(number):
-    return {"value": number}
-
-
-@app.route('/login', methods=["GET", "POST"])
-def login_page():
-
-    if request.method == "POST":
-
-        attempted_username = request.json['username']
-        attempted_password = request.json['password']
-        if attempted_username == "admin" and attempted_password == "password":
-            print({attempted_username, attempted_password})
-            return {"attempted_username": attempted_username, "attempted_password": attempted_password}
-
-        else:
-            return {"value": "Invalid credentials. Try Again."}
-
-    else:
-        return {"value": "trying to login?"}
 
 
 if __name__ == "__main__":
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from app import app, db
 from app.models import Employee, Role
-from flask import request
+from flask import request,abort, jsonify, g, url_for
 from datetime import datetime
+from flask_httpauth import HTTPBasicAuth
 import uuid
+import requests
+import xmltodict
+auth = HTTPBasicAuth()
 
 @app.route('/')
 @app.route('/index')
@@ -59,3 +63,77 @@ def deleteRole():
         return "role deleted successfully"
     else:
         return {"value": "trying to get user ?"}
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    print("aaaaaaaa",username_or_token)
+    employee = Employee.verify_auth_token(username_or_token)
+    if not employee:
+        # try to authenticate with email/password
+        employee = Employee.query.filter_by(email=username_or_token).first()
+        if not employee or not employee.verify_password(password):
+            return False
+    g.employee = employee
+    return True
+
+
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.employee.generate_auth_token(600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
+
+@app.route('/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, %s!' % g.employee.email})
+
+@app.route("/number/<number>")
+def get_number(number):
+    return {"value": number}
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login_page():
+
+    if request.method == "POST":
+
+        attempted_username = request.json['username']
+        attempted_password = request.json['password']
+        if attempted_username == "admin" and attempted_password == "password":
+            print({attempted_username, attempted_password})
+            return {"attempted_username": attempted_username, "attempted_password": attempted_password}
+
+        else:
+            return {"value": "Invalid credentials. Try Again."}
+
+    else:
+        return {"value": "trying to login?"}
+
+@app.route('/getAllTeams', methods=["GET"])
+def get_all_teams():
+    teams = []
+    response = requests.get(
+        'http://m7.tm00.com/tmpartnerservice/Services/PartnerDropdownlistService.svc/GetDropdownvaluesByHostIdAndListId/47/Team',
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": "18:E6LfjeMCP"
+        },
+    )
+
+    responseContentJson =  xmltodict.parse(response.content)
+    if responseContentJson["PartnerDDLValue"] is not None :
+        listItems = responseContentJson["PartnerDDLValue"]["ListItems"]["a:ListItem"]
+        for item in listItems :
+            teams.append(item["a:ListItemValue"])
+
+    return jsonify(teams)
+
+
+
+
+
+
+
